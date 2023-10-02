3f80845eb54f5aa195dc47bfb26912eb40391da4
saharking1298 <saharlan500@gmail.com>
2023-10-02 13:16:41 +0300

    New tab shortcuts no longer open in a new tab

--- src/newtab/newtab.js
+++ src/newtab/newtab.js
@@ -7,7 +7,7 @@ function render (data) {
         if (name.length > charLimit) {
             name = name.substring(0, charLimit) + "...";
         }
-        display.innerHTML += `<div class="shortcut-card"><a href="${item.url}" target="_blank"><div class="shortcut-image"><img src="https://s2.googleusercontent.com/s2/favicons?sz=32&domain_url=${item.url}" alt=${name}"></div><span>${name}</span></a></div>`;
+        display.innerHTML += `<div class="shortcut-card"><a href="${item.url}"><div class="shortcut-image"><img src="https://s2.googleusercontent.com/s2/favicons?sz=32&domain_url=${item.url}" alt=${name}"></div><span>${name}</span></a></div>`;
     }
 };
 
