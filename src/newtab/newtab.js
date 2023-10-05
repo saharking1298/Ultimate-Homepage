336ac69005fb07e20d2e4fa9444493c71d7b840c
saharking1298 <saharlan500@gmail.com>
2023-10-05 13:05:03 +0300

    Fixed missing images display in new tab

--- src/newtab/newtab.js
+++ src/newtab/newtab.js
@@ -1,21 +1,39 @@
+const display = document.getElementById("shortcuts-display");
 const charLimit = 18;
 
-function render (data) {
-    const display = document.getElementById("shortcuts-display");
-    for (let item of data) {
-        let name = item.name;
-        if (name.length > charLimit) {
-            name = name.substring(0, charLimit) + "...";
-        }
-        display.innerHTML += `<div class="shortcut-card"><a href="${item.url}"><div class="shortcut-image"><img src="https://s2.googleusercontent.com/s2/favicons?sz=32&domain_url=${item.url}" alt=${name}"></div><span>${name}</span></a></div>`;
+class App {
+    constructor () {
+        this.manager = new DataManager();
     }
-};
 
-async function run () {
-    const manager = new DataManager();
-    await manager.init();
+    render () {
+        const that = this;
 
-    render(manager.shortcuts);
+        for (let item of this.manager.shortcuts) {
+            let name = item.name;
+            if (name.length > charLimit) {
+                name = name.substring(0, charLimit) + "...";
+            }
+            display.innerHTML += `<div class="shortcut-card"><a href="${item.url}"><div class="shortcut-image"><img src="https://s2.googleusercontent.com/s2/favicons?sz=32&domain_url=${item.url}" alt=${name}"></div><span>${name}</span></a></div>`;
+        }
+    
+        const images = document.getElementsByTagName("img");
+        for (let i = 0; i < images.length; i++) {
+            images[i].onload = function () {
+                if (this.naturalWidth === 16 && this.naturalHeight === 16) {
+                    const parent = this.parentElement;
+                    this.remove();
+                    parent.innerHTML += `<div class="missing-image">${that.manager.shortcuts[i].name[0]}</div>`;
+                }
+            }
+        }
+    };
+
+    async run () {
+        await this.manager.init();
+        this.render();
+    }
 }
 
-run();
+const app = new App();
+app.run();
